bf20e9dd8d684dcc7c80f7808ca5793cb2bda994
Kevin Vasquez <kevinavasquez@gmail.com>
2017-11-15 17:57:05 -0600

    Added functionality
    initial

--- kevin/Bioinformatics/MaxAffinity.py
+++ kevin/Bioinformatics/MaxAffinity.py
@@ -3,6 +3,13 @@ import sequencing.acidToDNA as acidToDNA
 import random
 lookup = afinity.mfeDictionary
 
+'''This code will break up the imput sequence into synonymous codons and
+generates 3 sequence trains: 1. wt, 2.high, and 3. random. In this way, a
+random guided walk towards achieving the highest affinity variant of the imput
+sequence is achieved.
+'''
+
+#sequence = sequenceReading.readFile("ATGGCCGACTACAAGTGATAA")
 sequence = acidToDNA.swapBase("ATGGCCGACTACAAGGACGATGACGACAAGGGAGGATCCGACAAGCAGAAGAACGGCATCAAGGTGAACTTCAAGATCCGCCACAACATCGAGGACGGCAGCGTGCAGCTCGCCGACCACTACCAGCAGAACACCCCCATCGGCGACGGCCCCGTGCTGCTGCCCGACAACCACTACCTGAGCTACCAGTCCGCCCTGAGCAAAGACCCCAACGAGAAGCGCGATCACATGGTCCTGCTGGAGTTCGTGACCGCCGCCGGGATCACTCTCGGCATGGACGAGCTGTACAAGGAGCTCCATCATCATCATCATCACAGCAGCGGCCTGGTGCCGCGCGGCAGCCATGAATTCGGATCCGCTGGCTCCGCTGCTGGTTCTGGCGTCGACATGAGTGGAATACTGACGCGCTGGCGACAGTTTGGTAAACGCTACTTCTGGCCGCATCTCTTATTAGGGATGGTTGCGGCGAGTTTAGGTTTGCCTGCGCTCAGCAACGCCGCCGAACCAAACGCGCCCGCAAAAGCGACAACCCGCAACCACGAGCCTTCAGCCAAAGTTAACTTTGGTCAATTGGCCTTGCTGGAAGCGAACACACGCCGCCCGAATTCGAACTATTCCGTTGATTACTGGCATCAACATGCCATTCGCACGGTAATCCGTCATCTTTCTTTCGCAATGGCACCGCAAACACTGCCCGTTGCTGAAGAATCTTTGCCTCTTCAGGCGCAACATCTTGCATTACTGGATACGCTCAGCGCGCTGCTGACCCAGGAAAGGCACGCCGTCTGA","U")
 pos = 0
 WTsequence = ""
@@ -13,44 +20,22 @@ HIaff = 0
 RNDaff = 0
 #while you're one codon away from the stop codon, generate all combinations and
 #add them to the corresponding  WT, HI, or RND sequence, also store the affinity values
-while pos < len(sequence)-9:
+while pos < len(sequence)-6:
     codon1 = sequence[pos:pos+3]
     codon2 = sequence[pos+3:pos+6]
-    codon3 = sequence[pos+6:pos+9]
-    TemptAffHI = 0
-    TempAffRND = 0
 
 
     allpos = acidToDNA.CreateSixMer(codon1+codon2)
-    allpos2 = acidToDNA.CreateSixMer(codon2+codon3)
     aList = []
-    aList2 = []
     for i in allpos:
         aList.append(lookup[i])
 
-    for i in allpos2:
-        aList2.append(lookup[i])
 
     #for the wildtype sequence
     WTsequence += codon1
     WTaff += aList[0]
-
     #for the Maximum affinity sequence
-    MaxAffHI=0
-    indOfHI=0
-    keyi=-1
-    keyj=-1
-    for i in aList:
-        keyi+=1
-        keyj=-1
-        for j in aList2:
-            keyj+=1
-            TemptAffHI = i*j
-            if TemptAffHI>MaxAffHI:
-                MaxAffHI=TemptAffHI
-                indOfHI=keyi
-                indOfHij=keyj
-
+    indOfHI = aList.index(min(aList))
     HIsequence += allpos[indOfHI][0:3]
     HIaff += aList[indOfHI]
 
@@ -61,13 +46,12 @@ while pos < len(sequence)-9:
 
 
     pos += 3
-indOfHIj=0
+
 #add the second to last codon and stop codon
 #for wildtype
-WTsequence += codon2 + codon3 + sequence[pos+6:pos+9]
-HIsequence += allpos[indOfHI][3:6] + allpos2[indOfHIj][3:6] + sequence[pos+6:pos+9]
-RNDsequence += allpos[indOfRND][3:6] + sequence[pos+6:pos+9]
-
+WTsequence += codon2 + sequence[pos+3:pos+6]
+HIsequence += allpos[indOfHI][3:6] + sequence[pos+3:pos+6]
+RNDsequence += allpos[indOfRND][3:6] + sequence[pos+3:pos+6]
 
 #Calculate Affinity
 def CalculateAffinity(sequence):
@@ -93,3 +77,31 @@ print(acidToDNA.swapBase(WTsequence), "affinityCalculatedWT:", affWT)
 print(acidToDNA.swapBase(HIsequence),"affinityCalculatedHI:", affHI)
 print(acidToDNA.swapBase(RNDsequence),"affinityCalculatedRND:", affRND)
 print(sequence)
+
+
+
+
+'''
+aa1 = sequence[0:3]
+aa2 = sequence[3:6]
+
+allpos = acidToDNA.CreateSixMer(aa1+aa2)
+print("These are all of the possibilities: ", allpos)
+aList = []
+
+for i in allpos:
+    print(lookup[i])
+    aList.append(lookup[i])
+
+indOfMax = aList.index(max(aList))
+#if len(allpos) >
+#indOfRand =
+
+
+
+print("Affinity Values: ", aList)
+
+print(allpos[indOfMax][3:6])
+print("index of max", indOfMax)
+print(aList[1])
+'''
--- kevin/Bioinformatics/MaxMaxAffinity.py
+++ kevin/Bioinformatics/MaxMaxAffinity.py
@@ -3,7 +3,14 @@ import sequencing.acidToDNA as acidToDNA
 import random
 lookup = afinity.mfeDictionary
 
-sequence = acidToDNA.swapBase("ATGGCCGACTACAAGGACGATGACGACAAGGGAGGATCCGACAAGCAGAAGAACGGCATCAAGGTGAACTTCAAGATCCGCCACAACATCGAGGACGGCAGCGTGCAGCTCGCCGACCACTACCAGCAGAACACCCCCATCGGCGACGGCCCCGTGCTGCTGCCCGACAACCACTACCTGAGCTACCAGTCCGCCCTGAGCAAAGACCCCAACGAGAAGCGCGATCACATGGTCCTGCTGGAGTTCGTGACCGCCGCCGGGATCACTCTCGGCATGGACGAGCTGTACAAGGAGCTCCATCATCATCATCATCACAGCAGCGGCCTGGTGCCGCGCGGCAGCCATGAATTCGGATCCGCTGGCTCCGCTGCTGGTTCTGGCGTCGACATGAGTGGAATACTGACGCGCTGGCGACAGTTTGGTAAACGCTACTTCTGGCCGCATCTCTTATTAGGGATGGTTGCGGCGAGTTTAGGTTTGCCTGCGCTCAGCAACGCCGCCGAACCAAACGCGCCCGCAAAAGCGACAACCCGCAACCACGAGCCTTCAGCCAAAGTTAACTTTGGTCAATTGGCCTTGCTGGAAGCGAACACACGCCGCCCGAATTCGAACTATTCCGTTGATTACTGGCATCAACATGCCATTCGCACGGTAATCCGTCATCTTTCTTTCGCAATGGCACCGCAAACACTGCCCGTTGCTGAAGAATCTTTGCCTCTTCAGGCGCAACATCTTGCATTACTGGATACGCTCAGCGCGCTGCTGACCCAGGAAAGGCACGCCGTCTGA","U")
+'''This code will break up the imput sequence into synonymous codons and
+generates 3 sequence trains: 1. wt, 2.high, and 3. random. In this way, a
+random guided walk towards achieving the highest affinity variant of the imput
+sequence is achieved.
+'''
+
+#sequence = sequenceReading.readFile("ATGGCCGACTACAAGTGATAA")
+sequence = acidToDNA.swapBase("GCTGGCTCCGCTGCTGGTTCTGGCGAA","U")
 pos = 0
 WTsequence = ""
 HIsequence = ""
@@ -36,20 +43,7 @@ while pos < len(sequence)-9:
     WTaff += aList[0]
 
     #for the Maximum affinity sequence
-    MaxAffHI=0
-    indOfHI=0
-    keyi=-1
-    keyj=-1
-    for i in aList:
-        keyi+=1
-        keyj=-1
-        for j in aList2:
-            keyj+=1
-            TemptAffHI = i*j
-            if TemptAffHI>MaxAffHI:
-                MaxAffHI=TemptAffHI
-                indOfHI=keyi
-                indOfHij=keyj
+    indOfHI = aList.index(min(aList))
 
     HIsequence += allpos[indOfHI][0:3]
     HIaff += aList[indOfHI]
@@ -61,13 +55,12 @@ while pos < len(sequence)-9:
 
 
     pos += 3
-indOfHIj=0
+
 #add the second to last codon and stop codon
 #for wildtype
-WTsequence += codon2 + codon3 + sequence[pos+6:pos+9]
-HIsequence += allpos[indOfHI][3:6] + allpos2[indOfHIj][3:6] + sequence[pos+6:pos+9]
-RNDsequence += allpos[indOfRND][3:6] + sequence[pos+6:pos+9]
-
+WTsequence += codon2 + sequence[pos+3:pos+6]
+HIsequence += allpos[indOfHI][3:6] + sequence[pos+3:pos+6]
+RNDsequence += allpos[indOfRND][3:6] + sequence[pos+3:pos+6]
 
 #Calculate Affinity
 def CalculateAffinity(sequence):
@@ -85,11 +78,42 @@ affWT = CalculateAffinity(WTsequence)
 affHI = CalculateAffinity(HIsequence)
 affRND =CalculateAffinity(RNDsequence)
 
+lastcodon = affWT - WTaff
+print("lastcodon",lastcodon)
+
 #rewrite to T instead of U
-print(acidToDNA.swapBase(WTsequence), "affinityWT:", WTaff)
-print(acidToDNA.swapBase(HIsequence),"affinityHI:", HIaff)
-print(acidToDNA.swapBase(RNDsequence),"affinityRND:", RNDaff)
+print(acidToDNA.swapBase(WTsequence), "affinity:", WTaff+lastcodon)
+print(acidToDNA.swapBase(HIsequence),"affinity:", HIaff+lastcodon)
+print(acidToDNA.swapBase(RNDsequence),"affinity:", RNDaff)
 print(acidToDNA.swapBase(WTsequence), "affinityCalculatedWT:", affWT)
 print(acidToDNA.swapBase(HIsequence),"affinityCalculatedHI:", affHI)
 print(acidToDNA.swapBase(RNDsequence),"affinityCalculatedRND:", affRND)
 print(sequence)
+
+
+
+
+'''
+aa1 = sequence[0:3]
+aa2 = sequence[3:6]
+
+allpos = acidToDNA.CreateSixMer(aa1+aa2)
+print("These are all of the possibilities: ", allpos)
+aList = []
+
+for i in allpos:
+    print(lookup[i])
+    aList.append(lookup[i])
+
+indOfMax = aList.index(max(aList))
+#if len(allpos) >
+#indOfRand =
+
+
+
+print("Affinity Values: ", aList)
+
+print(allpos[indOfMax][3:6])
+print("index of max", indOfMax)
+print(aList[1])
+'''
